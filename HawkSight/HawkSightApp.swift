97d5583bc3d3d56ef767be945989ce9b036d5d23
Abraham Gonzalez <theabecaster0@gmail.com>
2025-11-14 21:54:14 -0500

    Initial Commit

---
+++ HawkSight/HawkSightApp.swift
@@ -0,0 +1,17 @@
+//
+//  HawkSightApp.swift
+//  HawkSight
+//
+//  Created by Abraham Gonzalez on 11/14/25.
+//
+
+import SwiftUI
+
+@main
+struct HawkSightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
